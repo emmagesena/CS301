de86199c12f01c23e716a7c6dcfe8593cb42e64a
Emma <emma.gesena@gmail.com>
2022-02-01 09:20:56 -0700

    Added more test functions

--- Assignment2-Runtime/Assignment2_Runtime.py
+++ Assignment2-Runtime/Assignment2_Runtime.py
@@ -1,6 +1,9 @@
 ## Assignment 2 ##
 
-## Functions ##
+import time
+import random
+
+## General Functions ##
 
 def generateDictionary(list):
     strList = [str(int) for int in list]
@@ -10,7 +13,77 @@ def generateDictionary(list):
     for i in range(len(list)):
         dict[strList[i]] = list[i]
 
-    print(dict)
+## Testing functions ## 
+
+def testList_Append(testList):
+    list = testList.copy()
+
+    t0 = time.time()
+    ## 
+    list.append(1)
+    ##
+    t1 = time.time()
+
+    return t1-t0
+
+def testList_insertFront(testList):
+    list = testList.copy()
+
+    t0 = time.time()
+    ##
+    list.insert(0,1)
+    ##
+    t1 = time.time()
+
+    return t1-t0
+
+
+
+'''
+Might want to edit testList_insertMiddle to make it more 
+efficient, the way I inserted in the middle of the
+list may be kind of dumb lol 
+'''
+
+def testList_insertMiddle(testList):
+    list = testList.copy()
+
+    t0 = time.time()
+    ##
+    middle = int(len(list)/2)
+    list.insert(middle, 1)
+    ##
+    t1 = time.time()
+
+    return t1-t0
+
+def testList_insertRandom(testList):
+    list = testList.copy()
+
+    t0 = time.time()
+    ##
+    list.insert(random.randint(0,len(list)-1),1)
+    ##
+    t1 = time.time()
+
+    return t1-t0
+
+def testList_deleteEnd(testList):
+    list = testList.copy()
+
+    t0 = time.time()
+    ##
+    ''' 
+    Apparently this was the most efficient way to delete
+    a list item directly 
+
+    ''' 
+    del list[-1]
+
+    ##
+    t1 = time.time()
+
+    return t1-t0
 
 ## Generate lists ##
 
@@ -30,4 +103,36 @@ dictTenThou = generateDictionary(tenthou)
 dictHundThou = generateDictionary(hundthou)
 dictMill = generateDictionary(mill)
 
-## 
+## Run tests ## 
+
+## List Append Tests ##
+
+
+'''
+Following code is just to check my test functions are
+running correctly, we will want to 
+use them in a more sophisticated way to collect data 
+'''
+#result = testList_Append(ten)
+#print(result)
+
+#result =  testList_Append(mill)
+#print(result)
+
+#result = testList_insertFront(ten)
+#print(result)
+
+#result = testList_insertFront(mill)
+#print(result)
+
+#result = testList_insertMiddle(ten)
+#print(result)
+
+#result = testList_insertMiddle(mill)
+#print(result)
+
+#result = testList_insertRandom(ten)
+#print(result)
+
+#result = testList_insertRandom(mill)
+#print(result)
